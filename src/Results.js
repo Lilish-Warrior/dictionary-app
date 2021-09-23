0efb4eee4650abe5ebc1b81758dd0a0c2acffcdb
Lilish-Warrior <87679658+Lilish-Warrior@users.noreply.github.com>
2021-09-23 12:29:42 +1000

    Created Phonetics component

--- src/Results.js
+++ src/Results.js
@@ -1,18 +1,31 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2 className="text-capitalize">{props.results.word}</h2>
-        {props.results.meanings.map(function (meaning, index) {
-          return (
-            <div key={index}>
-              <Meaning meaning={meaning} />
-            </div>
-          );
-        })}
+        <small>
+          {props.results.phonetics.map(function (phonetic, index) {
+            return (
+              <div>
+                <Phonetic phonetic={phonetic} />
+              </div>
+            );
+          })}
+        </small>
+        <br />
+        <div>
+          {props.results.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}{" "}
+        </div>
       </div>
     );
   } else {
